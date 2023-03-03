490ce5da15e8f194c2da7453e9125cb0442b1b9a
Subash Jirel <subashjrl@gmail.com>
2023-03-03 17:28:36 +0545

    added the js

---
+++ app.js
@@ -0,0 +1,19 @@
+let openBtn = document.getElementById('open-btn')
+let modalContainer = document.querySelector('#modal-container')
+let closeBtn = document.querySelector('#close-btn')
+
+//event listeners
+openBtn.addEventListener('click',function(){
+
+    modalContainer.style.display = 'block'
+})
+
+closeBtn.addEventListener('click',function() {
+    modalContainer.style.display= 'none'
+})
+
+window.addEventListener('click', function(e) {
+    if(e.target === modalContainer) {
+        modalContainer.style.display = 'none'
+    }
+})
